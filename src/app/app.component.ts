c1eaffc7456ada6e4ece7da45bde851f0b13f523
Alejo6648 <dajozavargas@gmail.com>
2024-11-06 20:26:33 -0500

    implementar botón para cambiar entre componentes TableStudents y TableCourses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableCoursesComponent } from './table-courses/table-courses.component';
+import { TableStudentsComponent } from './table-students/table-students.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableCoursesComponent, TableStudentsComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'list_courses_angular';
+  cursos: boolean = true;
+
+  hangleCambio() {
+    this.cursos = !this.cursos;
+  }
 }
