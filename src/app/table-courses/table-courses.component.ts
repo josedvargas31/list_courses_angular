b7c3e5ef9b71e5542bde177b910d73a1c6d178fc
Alejo6648 <dajozavargas@gmail.com>
2024-11-06 19:45:44 -0500

    Creación de los components table-students y table-courses

---
+++ src/app/table-courses/table-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-courses.component.html',
+  styleUrl: './table-courses.component.css'
+})
+export class TableCoursesComponent {
+
+}
---
+++ src/app/table-students/table-students.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-students',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-students.component.html',
+  styleUrl: './table-students.component.css'
+})
+export class TableStudentsComponent {
+
+}
