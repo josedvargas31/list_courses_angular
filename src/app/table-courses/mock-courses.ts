013e995a0445588cd11d52f777887ae44b4e1fa8
Alejo6648 <dajozavargas@gmail.com>
2024-11-07 18:44:26 -0500

    Add mock de alumnos y cursos

---
+++ src/app/table-courses/mock-courses.ts
@@ -0,0 +1,22 @@
+export const courses = [
+  {
+    materia: 'JavaScript',
+    dificultad: 9,
+    requiere: 'HTML & CSS',
+  },
+  {
+    materia: 'TypeScript',
+    dificultad: 7,
+    requiere: 'HTML, CSS & JS',
+  },
+  {
+    materia: 'Angular',
+    dificultad: 9,
+    requiere: 'HTML, CSS, JS & TS',
+  },
+  {
+    materia: 'React',
+    dificultad: 7,
+    requiere: 'HTML, CSS $ JS',
+  },
+];
---
+++ src/app/table-students/mock-students.ts
@@ -0,0 +1,20 @@
+export const students = [
+    {
+        nombre: 'Miguel Andres',
+        apellido: 'Perez Vargas',
+        promedio: 0.0
+
+    },
+    {
+        nombre: 'Nala',
+        apellido: 'Molina Ruiz',
+        promedio: 5
+
+    },
+    {
+        nombre: 'Patroquio Hunji',
+        apellido: 'Clemendo Patria',
+        promedio: 8
+
+    },
+];
